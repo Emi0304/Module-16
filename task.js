2349a3b81f63bc82eeadc54f63e09c01a299d235
Emi0304 <sadiaparvinemi4326@gmail.com>
2024-02-03 02:33:55 +0600

    problem solve

---
+++ task.js
@@ -0,0 +1,26 @@
+// **************** problem no 1 *******************************
+
+var totalMoney = 1000;
+var orangePrice = 300;
+var applePrice = 350;
+var backMoney = totalMoney - (orangePrice + applePrice);
+console.log(backMoney);
+
+
+// **************** problem no 2 ******************************* 
+
+var math = 80;
+var bangla = 70.25;
+var english = 88;
+var physics = 69.34;
+var biology = 78.88;
+var total = math + bangla + english + physics + biology;
+var average = total / 5 ;
+console.log(average.toFixed(2));
+
+// **************** problem no 3 ******************************* 
+
+var number = 119;
+console.log(number % 5);
+
+
